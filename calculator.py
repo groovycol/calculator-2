b2eb24827d116b42b6519eda0033561e7002c023
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 16:35:33 -0700

    mid-way through code review

--- calculator.py
+++ calculator.py
@@ -1,18 +1,12 @@
-"""
-calculator.py
-
-Using our arithmetic.py file from Exercise02, create the
-calculator program yourself in this file.
-"""
 
 from arithmetic import *
 
 
 def convert_tokens_to_int(user_input):
     """
-    converts user input 1-n elements to integers
+    converts digits in list to integers
 
-    leaves the 0th element as a str
+    leaves all other elements as strings; appends all elements to a new list.
     """
     new_user_input = []
     for word in user_input:
@@ -30,36 +24,36 @@ while True:
     #     read input
     input = raw_input("> ")
     #     tokenize input
-    new_list = input.split(" ")
+    split_list = input.split(" ")
     #change token elements 1-n to integers
-    token = convert_tokens_to_int(new_list)
+    tokens = convert_tokens_to_int(split_list)
     #     if the first token is 'q', quit
-    if token[0] == 'q' or token[0] == 'Q':
+    if tokens[0] == 'q' or tokens[0] == 'Q':
         break
     #     otherwise decide which math function to call based on the tokens we read
-    elif token[0] == '+':
-        result = add(num1=token[1], num2=token[2])
+    elif tokens[0] == '+':
+        result = add(num1=tokens[1], num2=tokens[2])
         print result
-    elif token[0] == '-':
-        result = subtract(token[1], token[2])
+    elif tokens[0] == '-':
+        result = subtract(tokens[1], tokens[2])
         print result
-    elif token[0] == '*':
-        result = multiply(token[1], token[2])
+    elif tokens[0] == '*':
+        result = multiply(tokens[1], tokens[2])
         print result
-    elif token[0] == '/':
-        result = divide(token[1], token[2])
+    elif tokens[0] == '/':
+        result = divide(tokens[1], tokens[2])
         print result
-    elif token[0] == 'square':
-        result = square(token[1])
+    elif tokens[0] == 'square':
+        result = square(tokens[1])
         print result
-    elif token[0] == 'cube':
-        result = cube(token[1])
+    elif tokens[0] == 'cube':
+        result = cube(tokens[1])
         print result
-    elif token[0] == 'pow':
-        result = pow(token[1], token[2])
+    elif tokens[0] == 'pow':
+        result = pow(tokens[1], tokens[2])
         print result
-    elif token[0] == 'mod':
-        result = mod(token[1], token[2])
+    elif tokens[0] == 'mod':
+        result = mod(tokens[1], tokens[2])
         print result
     else:
         print "error: do not understand input"
