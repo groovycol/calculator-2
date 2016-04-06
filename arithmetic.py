85fb97021681809544cde9e2452ab40e6d4ad847
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 16:13:36 -0700

    Working while loop, with function to convert digit strings to integers

---
+++ arithmetic.py
@@ -0,0 +1,63 @@
+def add(num1, num2):
+    """
+    Adds two numbers together.
+
+    """
+
+    return num1 + num2
+
+def subtract(num1, num2):
+    """
+    Returns the second number subtracted from the first
+
+    """
+
+    return num1 - num2
+
+def multiply(num1, num2):
+    """
+    Multiplies the two inputs together
+
+    """
+
+    return num1 * num2
+
+def divide(num1, num2):
+    """
+    Divides the first input by the second, returning a floating point
+
+    """
+
+    return float(num1) / float(num2)
+
+def square(num1):
+    """
+    Returns the square of the input
+
+    """
+
+    return num1 ** 2
+
+def cube(num1):
+    """
+    Returns the cube of the input
+
+    """
+    return num1 ** 3
+
+
+def power(num1, num2):
+    """
+    Raises the first integer to the power of the second.
+
+    """
+
+    return num1 ** num2
+
+
+def mod(num1, num2):
+    """
+    Returns the remainder when the first integer is divided by the second integer.
+
+    """
+    return num1 % num2
---
+++ calculator.py
@@ -0,0 +1,57 @@
+"""
+calculator.py
+
+Using our arithmetic.py file from Exercise02, create the
+calculator program yourself in this file.
+"""
+
+from arithmetic import *
+
+
+def convert_tokens_to_int(user_input):
+    """
+    converts user input 1-n elements to integers
+
+    leaves the 0th element as a str
+    """
+    new_user_input = []
+    for word in user_input:
+        if word.isdigit():
+            word = int(word)
+            new_user_input.append(word)
+        else:
+            new_user_input.append(word)
+    return new_user_input
+
+
+
+# repeat forever:
+while True:
+    #     read input
+    input = raw_input("> ")
+    #     tokenize input
+    new_list = input.split(" ")
+    #change token elements 1-n to integers
+    token = convert_tokens_to_int(new_list)
+    #     if the first token is 'q', quit
+    if token[0] == 'q' or token[0] == 'Q':
+        break
+    #     otherwise decide which math function to call based on the tokens we read
+    elif token[0] == '+':
+        result = add(num1=token[1], num2=token[2])
+        print result
+    elif token[0] == '-':
+        result = subtract(token[1], token[2])
+        print result
+    elif token[0] == '*':
+        result = multiply(token[1], token[2])
+        print result
+    elif token[0] == '/':
+        result = divide(token[1], token[2])
+        print result
+    elif token[0] == 'square':
+        result = square(token[1])
+        print result
+    else:
+        print "error: do not understand input"
+        break 
