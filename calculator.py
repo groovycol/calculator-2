5328dfc9c9517277915472e6b3f5d32305eb7ee0
Hackbright Student <info@hackbrightacademy.com>
2016-04-07 12:14:34 -0700

    Split user input into two variables; edited functions to align

--- calculator.py
+++ calculator.py
@@ -9,23 +9,22 @@ def convert_tokens_to_int(user_input):
     leaves all other elements as strings; appends all elements to a new list.
     """
     new_user_input = []
-    for word in user_input:
-        if word == '':
+    for item in user_input:
+        if item == '':
             pass
-        #elif word[0] !=
-        elif word.isdigit():
-            word = int(word)
-            new_user_input.append(word)
+        elif item.isdigit():
+            item = int(item)
+            new_user_input.append(item)
         else:
-            new_user_input.append(word)
+            new_user_input.append(item)
     return new_user_input
 
 # #def validate_ints( num_ints_needed, lst):
 #     if num_ints_needed ==1
-#         if token[1] is not an int
+#         if token[0] is not an int
 #         throw error
 #     elif num_ints_needed == 2
-#         if token[1] not an int or if token[2[]]
+#         if token[0] not an int or if token[2[]]
 
 
 
@@ -33,38 +32,40 @@ def convert_tokens_to_int(user_input):
 while True:
     #     read input
     original_input = raw_input("> ")
-    print original_input
+    #print original_input
     #remove leading and trailing spaces
     original_input = original_input.strip()
-    print original_input 
+    #print original_input 
     #     tokenize input
     split_list = original_input.split(" ")
-    print split_list
+    #print split_list
+    #Remove first element from list, assign to new variable. 
+    first_token = split_list.pop(0)
     #change token elements 1-n to integers
     tokens = convert_tokens_to_int(split_list)
-    print tokens
+    #print tokens
     #     if the first token is 'q', quit
-    if tokens[0] == 'q' or tokens[0] == 'Q':
+    if first_token == 'q' or first_token == 'Q':
         break
     #     otherwise decide which math function to call based on the tokens we read
-    elif tokens[0] == '+':
+    elif first_token == '+':
         #validate_ints(2, tokens)
-        print add(num1=tokens[1], num2=tokens[2])
-    elif tokens[0] == '-':
-        print subtract(num1=tokens[1], num2=tokens[2])
-    elif tokens[0] == '*':
-        print multiply(num1=tokens[1], num2=tokens[2])
-    elif tokens[0] == '/':
-        print divide(num1=tokens[1], num2=tokens[2])
-    elif tokens[0] == 'square':
-        print square(num1=tokens[1])
-    elif tokens[0] == 'cube':
+        print add(num1=tokens[0], num2=tokens[1])
+    elif first_token == '-':
+        print subtract(num1=tokens[0], num2=tokens[1])
+    elif first_token == '*':
+        print multiply(num1=tokens[0], num2=tokens[1])
+    elif first_token == '/':
+        print divide(num1=tokens[0], num2=tokens[1])
+    elif first_token == 'square':
+        print square(num1=tokens[0])
+    elif first_token == 'cube':
         #validate_ints(1, tokens)
-        print cube(num1=tokens[1])
-    elif tokens[0] == 'pow':
-        print pow(num1=tokens[1], num2=tokens[2])
-    elif tokens[0] == 'mod':
-        print mod(num1=tokens[1], num2=tokens[2])
+        print cube(num1=tokens[0])
+    elif first_token == 'pow':
+        print pow(num1=tokens[0], num2=tokens[1])
+    elif first_token == 'mod':
+        print mod(num1=tokens[0], num2=tokens[1])
     else:
         print "error: do not understand input"
         continue 
