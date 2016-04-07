57b90c66687d0480e5d0ff82dfd6e987af72234c
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 18:00:38 -0700

    some new ideas for solving invalid input

--- calculator.py
+++ calculator.py
@@ -10,12 +10,22 @@ def convert_tokens_to_int(user_input):
     """
     new_user_input = []
     for word in user_input:
-        if word.isdigit():
+        if word == '':
+            pass
+        #elif word[0] !=
+        elif word.isdigit():
             word = int(word)
             new_user_input.append(word)
         else:
             new_user_input.append(word)
     return new_user_input
+
+# #def validate_ints( num_ints_needed, lst):
+#     if num_ints_needed ==1
+#         if token[1] is not an int
+#         throw error
+#     elif num_ints_needed == 2
+#         if token[1] not an int or if token[2[]]
 
 
 
@@ -23,15 +33,22 @@ def convert_tokens_to_int(user_input):
 while True:
     #     read input
     original_input = raw_input("> ")
+    print original_input
+    #remove leading and trailing spaces
+    original_input = original_input.strip()
+    print original_input 
     #     tokenize input
     split_list = original_input.split(" ")
+    print split_list
     #change token elements 1-n to integers
     tokens = convert_tokens_to_int(split_list)
+    print tokens
     #     if the first token is 'q', quit
     if tokens[0] == 'q' or tokens[0] == 'Q':
         break
     #     otherwise decide which math function to call based on the tokens we read
     elif tokens[0] == '+':
+        #validate_ints(2, tokens)
         print add(num1=tokens[1], num2=tokens[2])
     elif tokens[0] == '-':
         print subtract(num1=tokens[1], num2=tokens[2])
@@ -42,6 +59,7 @@ while True:
     elif tokens[0] == 'square':
         print square(num1=tokens[1])
     elif tokens[0] == 'cube':
+        #validate_ints(1, tokens)
         print cube(num1=tokens[1])
     elif tokens[0] == 'pow':
         print pow(num1=tokens[1], num2=tokens[2])
